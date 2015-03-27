90b4c406e97ef1ed17e18854c0736ca94af11981
Budmore <j.mach@gaminglive.tv>
2015-03-27 13:53:16 +0100

    Init

---
+++ test/server.spec.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+
+describe('Contacts mangaer spec', function() {
+	it('should pass the test', function(done) {
+
+		assert.equal(1+1, 2);
+
+		done();
+	});
+});
