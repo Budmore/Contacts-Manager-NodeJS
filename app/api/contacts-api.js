ca46cf5ee58485ed4402e784a633736a02597932
JM <j.mach@budmore.pl>
2015-03-28 21:26:34 +0100

    Move Contacts API to separate file

---
+++ app/api/contacts-api.js
@@ -0,0 +1,147 @@
+// Database
+var ContactModel = require('../models/contact');
+
+var contacts = {
+
+	/**
+	 * Get all contacts from db
+	 *
+	 * Method: GET
+	 * http://budmore.pl/api/v1/contacts/
+	 *
+	 * @param  {object} req Request data
+	 * @param  {object} res Respond data
+	 * @return {array}
+	 */
+	getAll: function(req, res) {
+
+		ContactModel.find({}, function(err, doc) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+
+			res.json(doc);
+		});
+
+	},
+
+	/**
+	 * Create new contact
+	 *
+	 * Method: POST
+	 * http://budmore.pl/api/v1/contacts/
+	 *
+	 * @param  {object} req Request data
+	 * @param  {object} res Respond data
+	 */
+	create: function(req, res) {
+
+		var _contact = {
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			nickname: req.body.nickname,
+			notes: req.body.notes,
+			email: req.body.email,
+			url: req.body.url,
+			birthday: req.body.birthday,
+			nameday: req.body.nameday
+		};
+
+		var createContact = new ContactModel(_contact);
+
+		createContact.save(function(err, doc) {
+			if (err) {
+				// @todo diffrent status for diffrent type of error.
+				return res.status(500).send(err);
+			}
+
+			res.send(doc);
+
+		});
+	},
+
+	/**
+	 * Get one contact from db. Find it by id in the request params.
+	 *
+	 * Method: GET
+	 * http://budmore.pl/api/v1/contacts/:id
+	 *
+	 * @param  {object} req Request data
+	 * @param  {object} res Respond data
+	 * @return {object}
+	 */
+	getById: function(req, res) {
+		var _id = req.params.id;
+
+		ContactModel.findById(_id, function(err, doc) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+
+			res.send(doc);
+		});
+	},
+
+	/**
+	 * Update contact. Find it by id in the request params
+	 *
+	 * Method: PUT
+	 * http://budmore.pl/api/v1/contacts/:id
+	 *
+	 * @param  {object} req Request data
+	 * @param  {object} res Respond data
+	 * @return {object}
+	 */
+	updateById: function(req, res) {
+
+		var _id = req.params.id;
+
+		var updatedContact = req.body;
+		delete updatedContact._id;
+
+
+		ContactModel.findByIdAndUpdate(_id, {$set: updatedContact}, function(err, doc) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+
+			res.send(doc);
+
+		});
+	},
+
+	/**
+	 * Delete contact. Find it by id in the request params
+	 *
+	 * Method: DELETE
+	 * http://budmore.pl/api/v1/contacts/:id
+	 *
+	 * @param  {object} req Request data
+	 * @param  {object} res Respond data
+	 * @return {object}
+	 */
+	deleteById: function(req, res) {
+
+		var _id = req.params.id;
+
+		ContactModel.findByIdAndRemove(_id, function(err, doc) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+
+			res.status(200).send();
+		});
+	}
+
+
+
+};
+
+
+module.exports = {
+	getAll: contacts.getAll,
+	create: contacts.create,
+	getById: contacts.getById,
+	updateById: contacts.updateById,
+	deleteById: contacts.deleteById
+};
