74656ae219865c2c6cf087c82c2010516fefb6ef
Budmore <j.mach@budmore.pl>
2015-05-25 15:28:54 +0200

    add CROS middleware (for domain localhost:9000)

--- app/server.js
+++ app/server.js
@@ -43,13 +43,32 @@ new NodeCron({
 	start: true
 });
 
-/*
+
 if (process.env.DEVELOPMENT) {
 	start(port, function() {
 		console.log('working');
 	});
 }
-*/
+
+
+
+
+
+
+// MIDDLEWARES
+// -----------------------------------------------------------------------------
+
+//CORS
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+
+app.use(allowCrossDomain);
 
 // ROUTES
 // -----------------------------------------------------------------------------
@@ -73,3 +92,6 @@ router
 
 
 app.use('/api/v1', router);
+
+
+
